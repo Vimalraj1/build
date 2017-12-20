56ac5de0d91fc7e78f08bdb373afc66269527a39
mydongistiny <jaysonedson@gmail.com>
2017-12-19 21:26:02 -0800

    build: Set default art info
    Change-Id: I50f3720273e6503c3dd216af4fac10d1cb301fd3
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- target/product/runtime_libart.mk
+++ target/product/runtime_libart.mk
@@ -67,21 +67,13 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     dalvik.vm.dexopt.secondary=true \
     dalvik.vm.appimageformat=lz4
 
-# Different dexopt types for different package update/install times.
-# On eng builds, make "boot" reasons only extract for faster turnaround.
-ifeq (eng,$(TARGET_BUILD_VARIANT))
-    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-        pm.dexopt.first-boot=extract \
-        pm.dexopt.boot=extract
-else
-    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-        pm.dexopt.first-boot=quicken \
-        pm.dexopt.boot=verify
-endif
+# make "boot" reasons only extract for faster turnaround.
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    pm.dexopt.first-boot=extract \
+    pm.dexopt.boot=extract \
     pm.dexopt.install=quicken \
-    pm.dexopt.bg-dexopt=speed-profile \
-    pm.dexopt.ab-ota=speed-profile \
+    pm.dexopt.bg-dexopt=speed \
+    pm.dexopt.ab-ota=speed \
     pm.dexopt.inactive=verify \
     pm.dexopt.shared=speed
